40e4fcd253789f57af21f3212b58c22b2a84f27d
Michael Chi <kalschi@kalschi-macbookpro.roam.corp.google.com>
2019-12-05 11:55:11 +0800

    add taxibot sample

--- index.js
+++ index.js
@@ -15,66 +15,28 @@ exports.dialogflowFirebaseFulfillment = ((request, response) => {
     console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
     const agent = new WebhookClient({ request, response });
     
-    function welcome (agent) {
-      agent.add(`Welcome to my agent!`);
-    }
-  
-    function fallback (agent) {
-      agent.add(`I didn't understand`);
-      agent.add(`I'm sorry, can you try again?`);
-    }
-  
-    async function intent_translate(agent) {
-        const config = {
-            projectId: process.env.PROJECT_ID, 
-            location: process.env.LOCATION,
-            targetLanguageCode: GetLanguageCode(agent.parameters.translate_target_language),
-            keyFile: 'keys/service-account-key.json'
-        };
-        var translator = new Translator(config);
-        var metrics = new MetricsManager(config);
-        console.log('===> createTargetLanguageMetric() <===');
-        //await metrics.createTargetLanguageMetric();
-        await metrics.opencensusTargetLanguage(config.targetLanguageCode);
-        
-        var v = await translator.translateText(agent.parameters.translate_target_script);
-        //await metrics.targetLanguage(config.targetLanguageCode);
-        console.log('===> targetLanguage() <===');
-        agent.add(v);
-    }
-    // // Uncomment and edit to make your own intent handler
-    // // uncomment `intentMap.set('your intent name here', yourFunctionHandler);`
-    // // below to get this function to be run when a Dialogflow intent is matched
-    // function yourFunctionHandler(agent) {
-    //   agent.add(`This message is from Dialogflow's Cloud Functions for Firebase inline editor!`);
-    //   agent.add(new Card({
-    //       title: `Title: this is a card title`,
-    //       imageUrl: 'https://dialogflow.com/images/api_home_laptop.svg',
-    //       text: `This is the body text of a card.  You can even use line\n  breaks and emoji! 💁`,
-    //       buttonText: 'This is a button',
-    //       buttonUrl: 'https://docs.dialogflow.com/'
-    //     })
-    //   );
-    //   agent.add(new Suggestion(`Quick Reply`));
-    //   agent.add(new Suggestion(`Suggestion`));
-    //   agent.setContext({ name: 'weather', lifespan: 2, parameters: { city: 'Rome' }});
+    // function welcome (agent) {
+    //   agent.add(`Welcome to my agent!`);
+    // }
+    // function fallback (agent) {
+    //   agent.add(`I didn't understand`);
+    //   agent.add(`I'm sorry, can you try again?`);
     // }
   
-    // // Uncomment and edit to make your own Google Assistant intent handler
-    // // uncomment `intentMap.set('your intent name here', googleAssistantHandler);`
-    // // below to get this function to be run when a Dialogflow intent is matched
-    // function googleAssistantHandler(agent) {
-    //   let conv = agent.conv(); // Get Actions on Google library conv instance
-    //   conv.ask('Hello from the Actions on Google client library!'); // Use Actions on Google library
-    //   agent.add(conv); // Add Actions on Google library responses to your agent's response
-    // }
+    async function intent_RequestTaxi(agent) {
+      const config = {
+          projectId: process.env.PROJECT_ID, 
+          location: process.env.LOCATION,
+          date: agent.parameters['@sys.date'] ? new Date(agent.parameters['@sys.date']) : null,
+          time: agent.parameters['@sys.time'] ? new Date(agent.parameters['@sys.time']) : null,
+          location: agent.parameters['@sys.location']['street-address'] ? agent.parameters['@sys.location']['street-address'] : agent.parameters['@sys.location']['business-name'],
+          keyFile: 'keys/service-account-key.json'
+      };
+      agent.add(`You want to got to ${config.location} at ${config.time}`);
+    }
   
     // Run the proper function handler based on the matched Dialogflow intent name
     let intentMap = new Map();
-    intentMap.set('Default Welcome Intent', welcome);
-    intentMap.set('Default Fallback Intent', fallback);
-    intentMap.set('intent_translate', intent_translate);
-    // intentMap.set('<INTENT_NAME_HERE>', yourFunctionHandler);
-    // intentMap.set('<INTENT_NAME_HERE>', googleAssistantHandler);
+    intentMap.set('RequestTaxi', intent_RequestTaxi);
     agent.handleRequest(intentMap);
   });
