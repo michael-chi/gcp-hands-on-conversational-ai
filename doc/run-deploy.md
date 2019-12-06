55f8b56655dccf4f996501567402743c542b4569
Michael Chi <kalschi@kalschi-macbookpro.roam.corp.google.com>
2019-12-06 09:55:17 +0800

    test

--- doc/run-deploy.md
+++ doc/run-deploy.md
@@ -5,5 +5,5 @@
 ####    Deploy to Cloud Run
 
 ```bash
-# gcloud beta run deploy test2 --image asia.gcr.io/kalschi-demo-001/conversational-ai-demo:latest --platform managed --region asia-northeast1 --set-env-vars PROJECT_ID=kalschi-demo-001,LOCATION=global
+# gcloud beta run deploy dialogflow-demo-api --image asia.gcr.io/kalschi-demo-001/conversational-ai-demo:latest --platform managed --region asia-northeast1 --set-env-vars PROJECT_ID=kalschi-demo-001,LOCATION=global --service-account kalschi-dialogflow-serviceacco@kalschi-demo-001.iam.gserviceaccount.com
 ```
---
+++ tests/generate_jwt.js
@@ -0,0 +1,32 @@
+// Make sure to `npm install --save request-promise` or add the dependency to your package.json
+const request = require('request-promise');
+
+const receivingServiceURL = 'https://dialogflow-demo-api-fd5tyopnsa-an.a.run.app/';
+
+// Set up metadata server request
+// See https://cloud.google.com/compute/docs/instances/verifying-instance-identity#request_signature
+const metadataServerTokenURL = 'http://metadata/computeMetadata/v1/instance/service-accounts/default/identity?audience=';
+const tokenRequestOptions = {
+    uri: metadataServerTokenURL + receivingServiceURL,
+    headers: {
+        'Metadata-Flavor': 'Google'
+    }
+};
+
+// Fetch the token, then provide the token in the request to the receiving service
+request(tokenRequestOptions)
+  .then((token) => {
+    //console.log(token);
+    return request(receivingServiceURL).auth(null, null, true, token)
+  })
+  .then((response) => {
+    console.log(response);
+    return;
+    res.status(200).send(response);
+  })
+  .catch((error) => {
+    console.log(error);
+    return;
+    res.status(400).send(error);
+  });
+    
