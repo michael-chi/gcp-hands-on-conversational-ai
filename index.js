cdb9a4ecd78e66ea9e87e496293b58ef29d803b3
Michael Chi <kalschi@kalschi-macbookpro.roam.corp.google.com>
2019-12-07 13:46:06 +0800

    fix permission handler

--- index.js
+++ index.js
@@ -9,9 +9,8 @@ const {
 } = require('actions-on-google');
 
 const functions = require('firebase-functions');
-// Create an app instance
 const app = dialogflow();
-// Register handlers for Dialogflow intents
+
 app.intent('RequestTaxi', (conv, params) => {
   console.log('====>Request Taxi');
   console.log(JSON.stringify(conv));
@@ -26,18 +25,15 @@ app.intent('RequestTaxi', (conv, params) => {
       keyFile: 'keys/service-account-key.json'
   };  
   console.log(`You want to got to ${config.location} at ${config.time}`);
-  conv.ask(`You want to got to ${config.location} at ${config.time}`);
+  conv.ask(`你要到 ${config.location} 在 ${config.time}`);
 });
 
 app.intent('Default Welcome Intent', (conv) => {
   const permissions = ['NAME'];
-  //let context = 'To address you by name';
-  // Location permissions only work for verified users
   // https://developers.google.com/actions/assistant/guest-users
   if (conv.user.verification === 'VERIFIED') {
     // Could use DEVICE_COARSE_LOCATION instead for city, zip code
     permissions.push('DEVICE_PRECISE_LOCATION');
-    //context += ' and know your location';
   }
   let context = '';
   const options = {
@@ -46,45 +42,19 @@ app.intent('Default Welcome Intent', (conv) => {
   };
   conv.ask(new Permission(options));
 });
-app.intent('Default Welcome Intent - yes', (conv, params, confirmationGranted) => {
-  // Also, can access latitude and longitude
-  // const { latitude, longitude } = location.coordinates;
-  console.log('======> YES <=====');
 
+app.intent('Default Welcome Intent - yes', (conv, params, confirmationGranted) => {
+  //  ** This Event handler is triggered by Event setting in an Intent
   console.log(JSON.stringify(conv));
   const {location} = conv.device;
   const {name} = conv.user;
   if (confirmationGranted && name && location) {
-    conv.ask(`Okay ${name.display}, I see you're at ` +
+    conv.ask(`好的， ${name.display}, 我將您的上車地點設定為 ` +
       `${location.formattedAddress}`);
   } else {
-    conv.ask(`Looks like I can't get your information.`);
+    conv.ask(`很抱歉，我需要您的地址才能為您服務`);
+    conv.close();
   }
-  conv.ask(`Would you like to try another helper?`);
-  conv.ask(new Suggestions([
-    'Confirmation',
-    'DateTime',
-    'Place',
-  ]));
 });
 
-app.intent('Default Welcome Intent 2', (conv, params, confirmationGranted) => {
-      // Also, can access latitude and longitude
-      // const { latitude, longitude } = location.coordinates;
-  console.log('======> here');
-      const {location} = conv.device;
-      //const {name} = conv.user;
-      if (confirmationGranted && location) {
-        conv.ask(`Okay ${name.display}, I see you're at ` +
-          `${location.formattedAddress}`);
-      } else {
-        conv.ask(`Looks like I can't get your information.`);
-      }
-      conv.ask(`Would you like to try another helper?`);
-      conv.ask(new Suggestions([
-        'Confirmation',
-        'DateTime',
-        'Place',
-      ]));
-    });
 exports.agent = app;
