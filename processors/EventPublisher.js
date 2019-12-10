dbb5dbcae43ed27057e1edd24579ed695976eb42
Michael Chi <kalschi@kalschi-macbookpro.roam.corp.google.com>
2019-12-11 01:02:26 +0800

    fix message publish bug

--- processors/EventPublisher.js
+++ processors/EventPublisher.js
@@ -5,32 +5,10 @@ class EventPublisher {
 
     }
 
-    async getTopic(topicName) {
-        const pubsub = new PubSub();
-        try {
-            var topic = await pubsub.createTopic(topicName);
-            return topic;
-        } catch (ex) {
-            console.error(`[ERROR]Unable to getTopic ${topicName}`);
-            console.error(`[ERROR]${ex}`);
-            var topics = await pubsub.getTopics();
-            topics.forEach(topic => {
-                console.log(`[INFO]Checking ${topic} <=> ${topicName}`);
-                if (topic.name == topicName) {
-                    console.log('[INFO]Got IT!');
-                    return topic;
-                }
-            });
-            return null;
-        }
-    }
-
     async publish(topicName, stringEvent) {
         try {
-            var topic = await this.getTopic(topicName);
-            if (!topic) {
-                throw `Unable to create or get topic: ${topicName}`;
-            }
+            console.log(`[Info]Publishing to ${topicName}.`);
+            const pubsub = new PubSub();
             const dataBuffer = Buffer.from(stringEvent);
             const messageId = await pubsub.topic(topicName).publish(dataBuffer);
             console.log(`[Info]Message ${messageId} published.`);
