1abeae323ab335687cab39bd4640e149339846c3
Michael Chi <kalschi@kalschi-macbookpro.roam.corp.google.com>
2019-12-15 20:27:37 +0800

    add test data generator

---
+++ doc/dataflow-batch-ingestion.md
@@ -0,0 +1,26 @@
+##  Overview
+
+
+####    Steps
+
+-   把BigQuery table schema json檔案上傳到Pub/Sub Topic
+
+```bash
+gsutil mb gs://<STORAGE BUCKET>
+gsutil cp ./dod/schema-bq-flat.json gs://<STORAGE BUCKET>/schema-bq-flat.json
+```
+
+-   將Node.Js Script user defined function上傳
+```bash
+gsutil cp ../scripts/batch-jscript-handler.js gs://kaslchi-taxi-demo
+```
+-   將Storage Bucket中的檔案發佈到Pub/Sub
+
+```bash
+gcloud dataflow jobs run ingestion-batch \
+    --gcs-location gs://dataflow-templates/latest/Stream_GCS_Text_to_Cloud_PubSub \
+    --parameters \
+inputFilePattern=gs://kaslchi-taxi-demo/bookevent*.json,\
+outputTopic=projects/kalschi-demo-001/topics/kalschi-bot-event-publisher
+--region asia-east1
+```
--- tests/load-generator/bookdata_generator.js
+++ tests/load-generator/bookdata_generator.js
@@ -3,8 +3,11 @@ const fs = require('fs');
 const times = [4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 0, 1];
 const time_chances = [0.03, 0.05, 0.2, 0.3, 0.5, 0.6, 0.5, 0.4, 0.5, 0.6, 0.6, 0.6, 0.4, 0.4, 0.7, 0.7, 0.6, 0.8, 0.5, 0.5, 0.02, 0.02]
 const addresses = require('./data/simulation_addresses.json');
+const uuidv1 = require('uuid/v1');
 
-function random(max){
+const VIPs = [9000001, 9000002];
+
+function random(max) {
     return Math.floor(Math.random() * Math.floor(max));
 }
 
@@ -71,6 +74,16 @@ function buildCustomers(count) {
             unavailableUntil: null   //DateTime
         });
     }
+    //  VIP
+    for(var vip in VIPs){
+        customers.push({
+            id: vip,
+            chance: 1,
+            available: true,
+            unavailableUntil: null   //DateTime
+        });
+    }
+
     return customers;
 }
 
@@ -101,19 +114,22 @@ function buildTaxis(count) {
     return map;
 }
 
-function generateDailyTable(date, chanceTable, customers, taxis) {
+function generateDailyTable(date, chanceTable, customers, taxis, func) {
     var results = [];
 
     for (var i = 0; i < times.length; i++) {
         var value = time_chances[i];
         var key = times[i];
         for (var min = 0; min < 60; min += 5) {
-            var currentTime = new Date(date.getFullYear(), date.getMonth(), date.getDay(), key, min, 0);
-            var currentChangeOfTakingTaxiByTime = value;
+            var currentTime = new Date(date.getFullYear(), date.getMonth(), date.getDate(), key, min, 0);
+
+            //  People don't like to take TAXI on Sunday and Saturday
+            var currentChangeOfTakingTaxiByTime = currentTime.getDay() == 0 ? value / 3 : value;
+            var currentChangeOfTakingTaxiByTime = currentTime.getDay() == 6 ? value / 2 : value;
+            
             //  Is there anyone requesting a Taxi ?
             for (var k = 0; k < customers.length; k++) {
                 v = customers[k];
-                console.log(`[${currentTime}]Start simulating customer ${v.id}`);
                 //  Update this customer's status
                 if (v.unavailableUntil >= currentTime) {
                     v.available = true;
@@ -123,14 +139,14 @@ function generateDailyTable(date, chanceTable, customers, taxis) {
                 if (v.available && v.chance >= Math.random() && currentChangeOfTakingTaxiByTime >= Math.random()) {
                     //  I need a Taxi now
                     console.log(`${v.id} is requesting Taxi...`);
-                    
+
                     var taxi = getNextAvailableTaxi(taxis, currentTime);
                     if (!taxi) {
                         console.log(`No available Taxi found`);
                     } else if (taxi &&
                         taxi.chance * Math.random() >= Math.random() * Math.random()) {
                         console.log(`found an available Taxi:${taxi.plate} for ${v.id} at ${currentTime}`);
-                        
+
                         //  Got a Taxi
                         console.log(`Taxi ${taxi.plate} take customer ${v.id}`);
                         //  Generate result record
@@ -138,30 +154,46 @@ function generateDailyTable(date, chanceTable, customers, taxis) {
                         const origin = addresses[random(addresses.length)];
                         const destination = addresses[random(addresses.length)];
                         console.log(`from ${JSON.stringify(origin)} to ${JSON.stringify(destination)}`);
-                        
+
                         var result = {
+                            conversationId: uuidv1(),
                             time: currentTime,
-                            customer: k,
-                            plate: taxi.plate,
-                            from_address: origin.address,
-                            from_lontitude: origin.coordinates.lng,
+                            customer_hash: k,
+                            plate_no: taxi.plate,
+                            from_address: origin.address.split('區')[0],
+                            from_longitude: origin.coordinates.lng,
                             from_latitude: origin.coordinates.lat,
-                            to_address: destination.address,
-                            to_lontitude: destination.coordinates.lng,
+                            to_address: destination.address.split('區')[0],
+                            to_longitude: destination.coordinates.lng,
                             to_latitude: destination.coordinates.lat,
-                            distance: getDistance(origin.coordinates.lat,origin.coordinates.lng,destination.coordinates.lat,destination.coordinates.lng)
+                            distance_km: getDistance(origin.coordinates.lat, origin.coordinates.lng, destination.coordinates.lat, destination.coordinates.lng)
                         };
-                        results.push(result);
+                        func(result);
+                        //results.push(result);
                         //  Take 1 hour for this customer, update Taxi
                         taxi.available = false;
-                        var newTime = currentTime.setMinutes(currentTime.getMinutes() + 60);
+                        
+                        var newTime = new Date(currentTime.getFullYear(), currentTime.getMonth(), currentTime.getDate(),
+                                                            currentTime.getHours(),currentTime.getMinutes(), currentTime.getSeconds());
+                        newTime.setMinutes(currentTime.getMinutes() + 60);
                         taxi.unavailableUntil = new Date(newTime);
+                        
                         updateTaxi(taxis, taxi.plate, taxi);
 
                         console.log(`Updated status of Taxi[${taxi.plate}] to unavailable till ${taxi.unavailableUntil}`);
+                        
                         //  Update Cutomer
                         v.available = false;
-                        newTime = currentTime.setMinutes(currentTime.getMinutes() + 60 * Math.random(8));
+                        newTime = new Date(currentTime.getFullYear(), currentTime.getMonth(), currentTime.getDate(),
+                                            currentTime.getHours(),currentTime.getMinutes(), currentTime.getSeconds());
+                        
+                        if(VIPs.find(vip => vip == v.id))
+                        {
+                            console.log(`VIP ${v.id} took a Taxi at ${currentTime}`);
+                            newTime.setMinutes(currentTime.getMinutes() + 60 * random(2));
+                        }else{
+                            newTime.setMinutes(currentTime.getMinutes() + 60 * random(8));
+                        }
                         v.unavailableUntil = new Date(newTime);
                         //customers.set(v.id, v);
                         updateCustomer(customers, v.id, v);
@@ -170,7 +202,7 @@ function generateDailyTable(date, chanceTable, customers, taxis) {
             }
         }
     }
-    return results;
+    //return results;
 }
 
 function getDistance(lat1, lng1, lat2, lng2) {
@@ -189,6 +221,22 @@ var chanceTable = buildChanceTable(times, time_chances);
 var customers = buildCustomers(12561);
 var taxi = buildTaxis(876);
 
-const date = new Date(2019, 1, 1);
-var results = generateDailyTable(date, chanceTable, customers, taxi);
-fs.writeFile(`./data/book_simulation_output_${date.getFullYear()}-${date.getMonth()}-${date.getDay()}.json`, JSON.stringify(results), () => { });
+const y = 2019;
+const m = 1;
+var start = process.argv[2];
+var end = process.argv[3];
+
+for (var d = start; d <= end; d++) {
+    const date = new Date(y, m, d);
+    const fn = `./data/bookevents_${y}-${m}-${d}.json`;
+    if(!fs.existsSync(fn)){
+        fs.writeFileSync(fn,'');
+    }
+    generateDailyTable(date, chanceTable, customers, taxi,
+        (record) => {
+                console.log('===writting');
+                fs.appendFileSync(fn,JSON.stringify(record) + '\r\n',()=>{});
+            }
+        );
+    console.log(`successfully generated - ${fn}`);
+}
