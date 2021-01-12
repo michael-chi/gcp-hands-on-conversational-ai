96766879a6e1a51aae508ffc767cd864bbe39d01
Michael Chi <kalschi@google.com>
2021-01-12 10:31:07 +0800

    say-as

--- processors/intents/OnboardingProcess.js
+++ processors/intents/OnboardingProcess.js
@@ -22,13 +22,13 @@ module.exports = {
             if (!name) { missingSlots.push('user_name'); missing_prompt += '姓名,'; }
             if (!datetime) { missingSlots.push('date-time'); missing_prompt += '報到時間,'; }
             if (!user_dep) { missingSlots.push('user_dep'); missing_prompt += '部門,'; }
-            if (missing_prompt != '') {
-                conv.ask('請提供' + missing_prompt);
-            } else if (name == '王大一') {
+            if (name == '王大一') {
                 console.log('name is Invalid');
                 conv.ask(`${name}已經報到了`);
                 params['user_name'] = '';
                 conv.contexts.set('automation_new-hire_onboarding_dialog_params_user_dep', 1, {'user_name': ''});
+            } else if (missing_prompt != '') {
+                conv.ask('請提供' + missing_prompt);
             } else {
                 conv.ask(`<speak>了解, ${name}將於<say-as interpret-as="date" format="yyyymmdd" detail="1">${datetime}</say-as>向<say-as interpret-as="characters">${user_dep}</say-as>報到, 請問這個資訊正確嗎？</speak>`);
             }
