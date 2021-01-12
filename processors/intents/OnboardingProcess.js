0fdfea016652fb479a677bad6f91fee6dceeffd1
Michael Chi <kalschi@google.com>
2021-01-12 17:22:17 +0800

    test

--- processors/intents/OnboardingProcess.js
+++ processors/intents/OnboardingProcess.js
@@ -15,17 +15,17 @@ module.exports = {
         //  3.  Wait user confirmation
         app.intent('automation.new-hire.onboarding', async (conv, params) => {
 
-            try{
+            try {
                 console.log('====>conv.body.queryResult.intent');
-                console.log(conv.body.queryResult.intent.name);            
-            }catch{
+                console.log(conv.body.queryResult.intent.name);
+            } catch {
 
             }
 
             const intentName = conv.intent
-                    .trim()
-                    .toLowerCase()
-                    .replace(/\s/g, "_").replace(".","_");
+                .trim()
+                .toLowerCase()
+                .replace(/\s/g, "_").replace(".", "_");
             let intentNames = conv.body.queryResult.intent.name.split('/');
             let idDialogContext = `${intentNames[intentNames.length - 1]}_id_dialog_context`;
             let nameDialogContext = `${intentName}_dialog_context`;
@@ -35,54 +35,67 @@ module.exports = {
             let missingSlots = [];
             var missing_prompt = '';
 
-            const slotFillingRegex = /.*contexts\/(?<contextName>.*dialog_params.*)/;
-            let existingSlotFillingContexts = [];
-            for (const context of conv.contexts) {
-                const isSlotFillingContext = slotFillingRegex.test(context.name);
-                if (isSlotFillingContext) {
-                    const match = slotFillingRegex.exec(context.name);
-                    existingSlotFillingContexts.push(match.groups.contextName);
-                }
-            }
+            var paramaters = { 
+                'user_name': name, 
+                'user_name.original': name,
+                'user_dep':user_dep,
+                'user_dep.original':user_dep,
+                'date-time':datetime,
+                'date-time.original':datetime
+                };
+            // const slotFillingRegex = /.*contexts\/(?<contextName>.*dialog_params.*)/;
+            // let existingSlotFillingContexts = [];
+            // for (const context of conv.contexts) {
+            //     const isSlotFillingContext = slotFillingRegex.test(context.name);
+            //     if (isSlotFillingContext) {
+            //         const match = slotFillingRegex.exec(context.name);
+            //         existingSlotFillingContexts.push(match.groups.contextName);
+            //     }
+            // }
 
-            
+
             if (!name) {
                 missingSlots.push('user_name');
                 missing_prompt += '姓名,';
-                conv.contexts.set('automation_new-hire_onboarding_dialog_params_user_name', 1, { 'user_name': '' ,'user_name.original': ''});
-                conv.contexts.set('automationnew-hireonboarding-followup', 1, { 'user_name': '','user_name.original': '' });
-                conv.contexts.set(idDialogContext, 2, { 'user_name': '','user_name.original': '' });
-                conv.contexts.set(nameDialogContext, 2), { 'user_name': '','user_name.original': '' };
+                // conv.contexts.set('automation_new-hire_onboarding_dialog_params_user_name', 1, { 'user_name': '', 'user_name.original': '' });
+                // conv.contexts.set('automationnew-hireonboarding-followup', 1, { 'user_name': '', 'user_name.original': '' });
+                // conv.contexts.set(idDialogContext, 2, { 'user_name': '', 'user_name.original': '' });
+                // conv.contexts.set(nameDialogContext, 2), { 'user_name': '', 'user_name.original': '' };
                 conv.ask('請提供以下資訊：' + missing_prompt);
+                conv.followup('get_user_info', paramaters);
             }
             else if (!datetime) {
                 missingSlots.push('date-time');
                 missing_prompt += '報到時間,';
-                conv.contexts.set('automation_new-hire_onboarding_dialog_params_date-time', 1, { 'date-time': '' ,'date-time.original': ''});
-                conv.contexts.set('automationnew-hireonboarding-followup', 1, { 'date-time': '','date-time.original': ''});
-                conv.contexts.set(idDialogContext, 2, { 'date-time': '' ,'date-time.original': ''});
-                conv.contexts.set(nameDialogContext, 2, { 'date-time': '' ,'date-time.original': ''});
+                // conv.contexts.set('automation_new-hire_onboarding_dialog_params_date-time', 1, { 'date-time': '', 'date-time.original': '' });
+                // conv.contexts.set('automationnew-hireonboarding-followup', 1, { 'date-time': '', 'date-time.original': '' });
+                // conv.contexts.set(idDialogContext, 2, { 'date-time': '', 'date-time.original': '' });
+                // conv.contexts.set(nameDialogContext, 2, { 'date-time': '', 'date-time.original': '' });
                 conv.ask('請提供以下資訊：' + missing_prompt);
+                conv.followup('get_user_info', paramaters);
             }
             else if (!user_dep) {
-                missingSlots.push('user_dep'); 
+                missingSlots.push('user_dep');
                 missing_prompt += '部門,';
-                conv.contexts.set('automation_new-hire_onboarding_dialog_params_user_dep', 1, { 'user_dep': '' ,'user_dep.original': ''});
-                conv.contexts.set('automationnew-hireonboarding-followup', 1, { 'user_dep': '','user_dep.original': '' });
-                conv.contexts.set(idDialogContext, 2, { 'user_dep': '' ,'user_dep.original': ''});
-                conv.contexts.set(nameDialogContext, 2, { 'user_dep': '' ,'user_dep.original': ''});
+                // conv.contexts.set('automation_new-hire_onboarding_dialog_params_user_dep', 1, { 'user_dep': '', 'user_dep.original': '' });
+                // conv.contexts.set('automationnew-hireonboarding-followup', 1, { 'user_dep': '', 'user_dep.original': '' });
+                // conv.contexts.set(idDialogContext, 2, { 'user_dep': '', 'user_dep.original': '' });
+                // conv.contexts.set(nameDialogContext, 2, { 'user_dep': '', 'user_dep.original': '' });
                 conv.ask('請提供以下資訊：' + missing_prompt);
+                conv.followup('get_user_info', paramaters);
             }
             else if (name == '王大一') {
                 console.log('name is Invalid');
+
                 conv.ask(`${name}已經報到過了, 請提供正確的姓名`);
-                params['user_name'] = '';
-                conv.followup('get_user_info', { 'user_name': '' ,'user_name.original': ''});
+                conv.followup('get_user_info', paramaters);
+
+                // params['user_name'] = '';
                 // conv.contexts.set('automation_new-hire_onboarding_dialog_params_user_name', 1, { 'user_name': '','user_name.original': '' });
                 // conv.contexts.set('automationnew-hireonboarding-followup', 1, { 'user_name': '','user_name.original': '' });
                 // conv.contexts.set(idDialogContext, 2, { 'user_name': '' ,'user_name.original': ''});
                 // conv.contexts.set(nameDialogContext, 2, { 'user_name': '','user_name.original': '' });
-                
+
             } else {
                 conv.ask(`<speak>了解, ${name}將於<say-as interpret-as="date" format="yyyymmdd" detail="1">${datetime}</say-as>向<say-as interpret-as="characters">${user_dep}</say-as>報到, 請問這個資訊正確嗎？</speak>`);
             }
