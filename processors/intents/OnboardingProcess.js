8216d8857c0db62c5fe10b25ace5f6d74968a9ca
Michael Chi <kalschi@google.com>
2021-01-12 16:11:03 +0800

    test

--- processors/intents/OnboardingProcess.js
+++ processors/intents/OnboardingProcess.js
@@ -38,27 +38,27 @@ module.exports = {
                 missingSlots.push('user_name');
                 missing_prompt += '姓名,';
                 conv.contexts.set('automation_new-hire_onboarding_dialog_params_user_name', 1, { 'user_name': '' });
-                conv.contexts.set('automationnew-hireonboarding-followup', 1);
-                conv.contexts.set(idDialogContext, 2);
-                conv.contexts.set(nameDialogContext, 2);
+                conv.contexts.set('automationnew-hireonboarding-followup', 1, { 'user_name': '' });
+                conv.contexts.set(idDialogContext, 2, { 'user_name': '' });
+                conv.contexts.set(nameDialogContext, 2), { 'user_name': '' };
                 conv.ask('請提供以下資訊：' + missing_prompt);
             }
             else if (!datetime) {
                 missingSlots.push('date-time');
                 missing_prompt += '報到時間,';
                 conv.contexts.set('automation_new-hire_onboarding_dialog_params_date-time', 1, { 'date-time': '' });
-                conv.contexts.set('automationnew-hireonboarding-followup', 1);
-                conv.contexts.set(idDialogContext, 2);
-                conv.contexts.set(nameDialogContext, 2);
+                conv.contexts.set('automationnew-hireonboarding-followup', 1, { 'date-time': '' });
+                conv.contexts.set(idDialogContext, 2, { 'date-time': '' });
+                conv.contexts.set(nameDialogContext, 2, { 'date-time': '' });
                 conv.ask('請提供以下資訊：' + missing_prompt);
             }
             else if (!user_dep) {
                 missingSlots.push('user_dep'); 
                 missing_prompt += '部門,';
                 conv.contexts.set('automation_new-hire_onboarding_dialog_params_user_dep', 1, { 'user_dep': '' });
-                conv.contexts.set('automationnew-hireonboarding-followup', 1);
-                conv.contexts.set(idDialogContext, 2);
-                conv.contexts.set(nameDialogContext, 2);
+                conv.contexts.set('automationnew-hireonboarding-followup', 1, { 'user_dep': '' });
+                conv.contexts.set(idDialogContext, 2, { 'user_dep': '' });
+                conv.contexts.set(nameDialogContext, 2, { 'user_dep': '' });
                 conv.ask('請提供以下資訊：' + missing_prompt);
             }
             else if (name == '王大一') {
@@ -66,9 +66,9 @@ module.exports = {
                 conv.ask(`${name}已經報到過了, 請提供正確的姓名`);
                 params['user_name'] = '';
                 conv.contexts.set('automation_new-hire_onboarding_dialog_params_user_name', 1, { 'user_name': '' });
-                conv.contexts.set('automationnew-hireonboarding-followup', 1);
-                conv.contexts.set(idDialogContext, 2);
-                conv.contexts.set(nameDialogContext, 2);
+                conv.contexts.set('automationnew-hireonboarding-followup', 1, { 'user_name': '' });
+                conv.contexts.set(idDialogContext, 2), { 'user_name': '' };
+                conv.contexts.set(nameDialogContext, 2, { 'user_name': '' });
                 
             } else {
                 conv.ask(`<speak>了解, ${name}將於<say-as interpret-as="date" format="yyyymmdd" detail="1">${datetime}</say-as>向<say-as interpret-as="characters">${user_dep}</say-as>報到, 請問這個資訊正確嗎？</speak>`);
