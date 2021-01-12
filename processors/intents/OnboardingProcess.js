cabd280a8da0a40f0e9c6daf5ac8b10b302b406d
Michael Chi <kalschi@google.com>
2021-01-12 09:09:29 +0800

    test

--- processors/intents/OnboardingProcess.js
+++ processors/intents/OnboardingProcess.js
@@ -24,6 +24,10 @@ module.exports = {
             if (!user_dep) { missingSlots.push('user_dep'); missing_prompt += '部門,'; }
             if (missing_prompt != '') {
                 conv.ask('請提供' + missing_prompt);
+            } else if (name == '王大一') {
+                conv.ask(`${name}已經onboard了`);
+                params['user_name'] = '';
+                app.setContext({'name':'automation_new-hire_onboarding_dialog_params_user_dep', 'lifespan': 1});
             } else {
                 conv.ask('好的！');
             }
