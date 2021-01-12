18c04512c95f06716e9952e823ac1b47dff25181
Michael Chi <kalschi@google.com>
2021-01-12 10:22:46 +0800

    say-as

--- processors/intents/OnboardingProcess.js
+++ processors/intents/OnboardingProcess.js
@@ -30,7 +30,7 @@ module.exports = {
                 params['user_name'] = '';
                 conv.contexts.set('automation_new-hire_onboarding_dialog_params_user_dep', 1, {'user_name': ''});
             } else {
-                conv.ask(`<speak>了解, ${name}<say-as interpret-as="date" format="yyyymmdd" detail="1">${datetime}</say-as>向${user_dep}報到, 請問這個資訊正確嗎？</speak>`);
+                conv.ask(`<speak>了解, ${name}<say-as interpret-as="date" format="yyyymmdd" detail="1">${datetime}</say-as>向<say-as interpret-as="characters">${user_dep}</say-as>報到, 請問這個資訊正確嗎？</speak>`);
             }
         });
     }
